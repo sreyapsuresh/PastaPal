c2e7aa26451dd93a2ee07c6f8eaf118eaf49b363
sreyapsuresh <sreyapsuresh@gmail.com>
2024-03-20 23:15:47 -0500

    Initial Commit

---
+++ PastaPal/PastaPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PastaPalApp.swift
+//  PastaPal
+//
+//  Created by Sreya Suresh on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct PastaPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
