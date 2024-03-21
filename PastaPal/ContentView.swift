7934762bd316d8e80a70fd08808a30b31e7f253f
sreyapsuresh <sreyapsuresh@gmail.com>
2024-03-20 23:33:04 -0500

    Developed PastaPal

--- PastaPal/ContentView.swift
+++ PastaPal/ContentView.swift
@@ -1,24 +1,66 @@
-//
 //  ContentView.swift
 //  PastaPal
-//
-//  Created by Sreya Suresh on 3/20/24.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+    let recipes: [Recipe] = [
+        Recipe(name: "Pasta Carbonara", ingredients: ["Spaghetti", "Eggs", "Bacon", "Parmesan Cheese"], quantities: [1, 2, 3, 1]),
+        Recipe(name: "Chicken Alfredo", ingredients: ["Fettuccine", "Chicken Breast", "Heavy Cream", "Parmesan Cheese"], quantities: [1, 1, 1, 1]),
+        Recipe(name: "Pasta Primavera", ingredients: ["Penne Pasta", "Cherry Tomatoes", "Olive Oil", "Parmesan Cheese"], quantities: [1, 2, 3, 1]),
+        Recipe(name: "Pasta Aglio e Olio", ingredients: ["Spaghetti", "Garlic", "Olive Oil", "Red Pepper Flakes"], quantities: [1, 4, 3, 2]),
+    ]
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(recipes, id: \.name) { recipe in
+                    NavigationLink(destination: RecipeDetailView(recipe: recipe)) {
+                        Text(recipe.name)
+                    }
+                }
+            }
+            .navigationTitle("PastaPal")
+        }
+    }
+}
+
+// This is a new view!!!
+struct RecipeDetailView: View {
+    @State private var multiplier = 1
+    
+    // Allows for Recipe's contents to be accessed here
+    let recipe: Recipe
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List {
+                ForEach(0..<recipe.ingredients.count, id: \.self) { index in
+                    HStack {
+                        Text(recipe.ingredients[index])
+                        Spacer()
+                        Text("\(recipe.quantities[index] * multiplier)x")
+                    }
+                }
+            }
+            
+            HStack {
+                Button("1x") { multiplier = 1 }
+                Button("2x") { multiplier = 2 }
+                Button("3x") { multiplier = 3 }
+            }
+            .padding()
         }
-        .padding()
+        .navigationTitle(recipe.name)
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct Recipe {
+    let name: String
+    let ingredients: [String]
+    let quantities: [Int]
 }
